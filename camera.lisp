7f2b1382c85a24dd1380df447da8a9d0d421c79b
Daniel Litvak <dlitvak98@protonmail.com>
2022-09-21 14:39:11 +0200

    (CAMERA): Start proper camera abstraction

--- camera.lisp
+++ camera.lisp
@@ -5,16 +5,47 @@
     :accessor pos
     :initarg :pos
     :initform (vec 0 0 0))
-   (pitch
-    :accessor pitch
-    :initarg :pitch
-    :initform 0.0
-    :documentation "Camera pitch in degrees")
-   (yaw
-    :accessor yaw
-    :initarg :yaw
-    :initform 0
-    :documentation "Camera yaw in degrees")))
+   (front
+    :accessor front
+    :initarg :front
+    :initform (vec 0 0 1))
+   (up
+    :accessor up
+    :initarg :up
+    :initform (vec 0 1 0))
+   (speed
+    :accessor speed
+    :initarg :speed
+    :initform 1.0
+    :documentation "Camera speed in \"units\" per second")))
+
+(defmacro with-camera-props ((pitch yaw) camera &rest body)
+  (let ((front-var (gensym)))
+    `(with-slots ((,front-var front)) ,camera
+       (let ((,pitch (asin (vy ,front-var)))
+	     (,yaw (vangle +vz+ (vx_z ,front-var))))
+	 ,@body))))
+
+(defclass fps-camera (camera)
+  (can-fly
+   :accessor can-fly
+   :initarg :can-fly
+   :initform t
+   :documentation "If true, forward moves camera in looking direction.
+If false, forward moves camera forward at same height"))
+
+(defclass top-down-camera (camera)
+  (min-height
+   :accessor min-height
+   :initarg :min-height
+   :initform 0.0))
+
+(defgeneric handle-keyboard (camera key dt))
+
+(defmethod handle-keyboard ((camera fps-camera) key dt)
+  (with-slots (speed) camera
+    (let ((speed (* speed dt)))
+      (with-camera-props (pitch yaw) camera
 
 (defmethod view-matrix ((camera camera))
   (with-slots (pos pitch yaw) camera
